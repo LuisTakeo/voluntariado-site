edaab8a4d2123518cbb659c5c836375ab9fcc51e
LuisTakeo <luistakeo@hotmail.com>
2023-08-29 18:19:03 -0300

    Criado página de serviços

---
+++ src/views/servicos.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="/pgprincipal.css" /> -->
+    <title>Bom Vizinho | Serviços</title>
+    <!-- Customized CSS -->
+
+    <!-- <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css" /> -->
+
+    <!-- Principal CSS -->
+
+    <link rel="stylesheet" href="../../pgprincipal.css" />
+    <link rel="stylesheet" href="../../src/css/servicos/servicos.css" />
+
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Font Styles -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header class="header">
+        <h2 class="header__title">Seja bem vindo, visitante!</h2>
+        <img src="../img/logo.jpg" alt="">
+    </header>
+    <div class="container-servicos">
+
+        <main class="main">
+            <h3>O que deseja hoje?</h3>
+            <section class="main__servicos">
+                <app-card titleCard="Trocar lampada" description="Serviço realizado por um voluntário para troca de lampada" />
+            </section>
+        </main>
+    </div>
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top custom "><i class="bi bi-arrow-up"></i></a>
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <script src="index.js"></script>
+</body>
+
+</html>
